fe6f7a37356f70d7dccc1ef53b90b63efe895ab7
majo-barroso <123590673+majo-barroso@users.noreply.github.com>
2023-07-11 12:45:26 -0500

    Initial commit

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+var lienzo1 = document.getElementById("lienzo");
+var papel1 = lienzo1.getContext("2d");
+var lienzo2 = document.getElementById("canvas2");
+var papel2 = lienzo2.getContest("2d");
+var imagen1 = new Image();
+imagen1.src = "";
